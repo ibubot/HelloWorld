3f4a02f524c4638d7b07fe5d0e12b4b59bd39c4e
azheng36 <azheng36@gatech.edu>
2016-10-20 17:49:52 -0400

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
   cout<<"Hiya world, Im Rick Harrison and this is my pawn shop"<<endl;
-  cout<<"I'm Ibrahim and I like poop."<<endl;
+  cout<<"I'm Ibrahim and I like pooop."<<endl;
   return 0;
 }
